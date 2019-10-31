4483f9086077e0be923e45667d3eda55cfe8d70d
aiden <2146405507@qq.com>
2019-10-31 10:13:34 +0800

    cmbc

--- shadowsocksr-speed.py
+++ shadowsocksr-speed.py
@@ -18,7 +18,7 @@ import socks
 default_socket = socket.socket
 
 test_option={}
-test_option ['ping']=test_option ['network']=test_option ['speed']= test_option ['abc']= test_option ['icbc']= test_option ['cmb']= test_option ['ccb']= test_option ['ceb']= test_option ['boc']=False
+test_option ['ping']=test_option ['network']=test_option ['speed']= test_option ['abc']= test_option ['icbc']= test_option ['cmb']= test_option ['ccb']= test_option ['ceb']= test_option ['boc']= test_option ['cmbc']=False
 max_cols=0
 # 访问 abc 网页加载时间大于设置时间直接退出不进行测速.解决高延迟的节点加载网页太慢问题
 abc_timeout=10
@@ -160,7 +160,7 @@ def TestOption(screen):
                 if test_select[3]:
                     test_option['abc'] = True
                 if Option==0:
-                    test_option['abc'] = test_option['ping'] = test_option['speed'] = test_option['network'] = test_option['icbc'] = test_option['cmb'] = test_option['ccb'] = test_option['ceb'] = test_option['boc'] =True
+                    test_option['abc'] = test_option['ping'] = test_option['speed'] = test_option['network'] = test_option['icbc'] = test_option['cmb'] = test_option['ccb'] = test_option['ceb'] = test_option['boc'] = test_option['cmbc'] =True
                 break
             if key in [32, curses.KEY_RIGHT, curses.KEY_LEFT]:
                 if (Option != 0):
@@ -393,6 +393,19 @@ def connect_ssr(ssr):
                     result['state'] = "Success"
                 else:
                     result['boc'] = 0
+                    result['state'] = "Fail"
+
+            if test_option['cmbc']:
+                start = time.time()
+                r = requests.get('http://www.cmbc.com.cn/', timeout=15)
+                if r.status_code == 200:
+                    end = time.time()
+                    print("cmbc_test:", (end - start) * 1000)
+                    t = end - start
+                    result['cmbc'] = int(round(t * 1000))
+                    result['state'] = "Success"
+                else:
+                    result['cmbc'] = 0
                     result['state'] = "Fail"
         return result
 
